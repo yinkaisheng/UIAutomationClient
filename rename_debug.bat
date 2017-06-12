7add24a1423dd8b898a90ef433d15d4d5003cf5d
yinkaisheng@foxmail.com <yinkaisheng@foxmail.com>
2017-06-12 16:55:23 +0800

    modify version

--- rename_debug.bat
+++ rename_debug.bat
@@ -1,5 +1,4 @@
-move UIAutomationClient_Win32_Debug.dll UIAutomationClientX86.dll
-move UIAutomationClient_x64_Debug.dll   UIAutomationClientX64.dll
+del bin\UIAutomationClientX*.dll
 
 move bin\UIAutomationClient_Win32_Debug.dll bin\UIAutomationClientX86.dll
 move bin\UIAutomationClient_x64_Debug.dll   bin\UIAutomationClientX64.dll
--- rename_release.bat
+++ rename_release.bat
@@ -1,5 +1,4 @@
-move UIAutomationClient_Win32_Release.dll UIAutomationClientX86.dll
-move UIAutomationClient_x64_Release.dll   UIAutomationClientX64.dll
+del bin\UIAutomationClientX*.dll
 
 move bin\UIAutomationClient_Win32_Release.dll bin\UIAutomationClientX86.dll
 move bin\UIAutomationClient_x64_Release.dll   bin\UIAutomationClientX64.dll
